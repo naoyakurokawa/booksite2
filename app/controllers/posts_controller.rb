917170c12421b6990e6204920cacfa08d4104587
Naoya Kurokawa <kurokawa14@gmail.com>
2018-11-24 06:40:44 +0000

    post add

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.all
+  end
+
+  def new
+    @post = Post.new
+  end
+  
+  def show
+    @post = Post.find_by(id:params[:id])
+  end
+
+  def create
+    @post = Post.new(post_params)
+
+    if @post.save
+      redirect_to root_url, notice: 'Add Post'
+    else
+      render :new
+    end
+  end
+
+  private
+  def post_params
+    params.fetch(:post, {}).permit(:book_title, :image)
+  end
+  
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/index'
+  get 'posts/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root "users#index"
-    resources :users, only: [:new, :create]
+    resources :users
+    resources :posts
 end
